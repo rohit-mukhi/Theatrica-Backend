f24b1e1ab1c5aa6451b79bac8d18476524022cc5
Rohit Mukhi <132041433+rohit-mukhi@users.noreply.github.com>
2025-08-27 21:50:00 +0530

    Update reviews.route.js

--- api/reviews.route.js
+++ api/reviews.route.js
@@ -4,7 +4,7 @@ import ReviewsCtrl from "./reviews.controller.js"
 const router = express.Router();
 
 router.route("/movie/:id").get(ReviewsCtrl.apiGetReview); 
-router.route("/new").post(ReviewsCtrl.apiPostreview);
+router.route("/new").post(ReviewsCtrl.apiPostReview);
 router.route("/:id").put(ReviewsCtrl.apiUpdateReview).delete(ReviewsCtrl.apiDeleteReview);
 
 export default router;
