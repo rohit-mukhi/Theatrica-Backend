344aa4e672d80a7de85c4db0be0735458ae8f3c1
Rohit Mukhi <132041433+rohit-mukhi@users.noreply.github.com>
2025-08-28 12:20:03 +0530

    Update reviews.route.js

--- api/reviews.route.js
+++ api/reviews.route.js
@@ -1,7 +1,13 @@
 import express from "express";
 import ReviewsCtrl from "./reviews.controller.js"
+import cors from 'cors';
 
 const router = express.Router();
+router.use(cors({
+  origin: 'https://theatrica.netlify.app',
+  methods: ['GET', 'POST', 'OPTIONS'],
+  allowedHeaders: ['Content-Type'],
+}));
 
 router.route("/movie/:id").get(ReviewsCtrl.apiGetReview); 
 router.route("/new").post(ReviewsCtrl.apiPostreview);
