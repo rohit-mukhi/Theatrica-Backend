d3fc6718eee25820f26d366965a0e951435c3cfb
Rohit Mukhi <132041433+rohit-mukhi@users.noreply.github.com>
2025-10-06 17:45:24 +0530

    Update reviews.route.js

--- api/reviews.route.js
+++ api/reviews.route.js
@@ -1,10 +1,17 @@
 import express from "express";
 import ReviewsCtrl from "./reviews.controller.js"
+import UserCtrl from "./Users.controller.js";
 
 const router = express.Router();
 
 router.route("/movie/:id").get(ReviewsCtrl.apiGetReview); 
 router.route("/new").post(ReviewsCtrl.apiPostreview);
 router.route("/:id").get(ReviewsCtrl.apiGetReview).put(ReviewsCtrl.apiPostreview).delete(ReviewsCtrl.apiDeleteReview);
+
+router.route("/newUser").post(UserCtrl.apiAddUser);
+router.route("/getUser/:username").get(UserCtrl.apiGetUser);
+router.route("/authenticate").post(UserCtrl.apiCheckPassword);
+router.route("/updateProfilePic/:username").put(UserCtrl.apiUpdateProfilePic);
+router.route("/removeProfilePic/:username").put(UserCtrl.apiDeleteProfilePic);
 
 export default router;
